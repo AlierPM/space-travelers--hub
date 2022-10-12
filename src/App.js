bd79a31d481bdad34c5a4b4b8abfe8ada3b86e80
Alier Philip <alierditmaguet@gmail.com>
2022-10-12 22:57:42 +0200

    Add logo and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 // Import rocket, mission, dragon and profile for the navlinks to function.
 import Rocket from './components/rocket/rocket';
 import Dragon from './components/dragon/dragon';
 import Mission from './components/mission/mission';
+import Profile from './components/myProfile/Profile';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
