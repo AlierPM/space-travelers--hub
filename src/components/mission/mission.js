632ec7c5db3bb3d3620bd0d41a3178712ce7b4ba
Petro Lesapiti <petrolesapiti@gmail.com>
2022-10-12 12:39:20 +0300

    Create a structure of the rocket module

---
+++ src/components/mission/mission.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Mission = () => (
+  <>
+    <h2>Mission Page</h2>
+  </>
+);
+
+export default Mission;
---
+++ src/components/rocket/rocket.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Rocket = () => (
+  <>
+    <h2>Rocket Page</h2>
+  </>
+);
+
+export default Rocket;
