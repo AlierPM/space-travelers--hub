f7d4b66616347fddd54e14088759a12a094c6fbe
Petro Lesapiti <petrolesapiti@gmail.com>
2022-10-12 09:52:48 +0300

    Create header navigation bar

--- src/App.js
+++ src/App.js
@@ -1,56 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-
+import { Link, Route, Routes } from "react-router-dom";
+// Import rocket, mission, dragon and profile for the navlinks to function.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+    <div>
+      <header className="panel-bg">
+        <div className="navigation">
+          <span className="Logo-Text">Space Travelers' Hub</span>
+          <ul>
+            <li>
+              <Link to="/rocket" className="Mission Text-Style-3">
+                Rocket
+              </Link>
+            </li>
+            <li>
+              <Link to="/mission" className="Mission Text-Style-3">
+                Missions
+              </Link>
+            </li>
+            <li>
+              <Link to="/dragon" className="Myprofile Text-Style-3">
+                Dragon
+              </Link>
+            </li>
+            <li>
+              <Link to="/profile" className="Myprofile Text-Style-3">
+                My Profile
+              </Link>
+            </li>
+          </ul>
+        </div>
       </header>
+      <main>
+        <Routes>
+          <Route path="/rocket" element={<Rocket />} />
+          <Route path="/mission" element={<Mission />} />
+          <Route path="/dragon" element={<Dragon />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
 }
