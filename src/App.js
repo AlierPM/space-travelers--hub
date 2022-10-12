2b0f177933a4aaa6ee6f9a855134a82369484c2c
Alier Philip <alierditmaguet@gmail.com>
2022-10-12 22:57:42 +0200

    Add logo and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 // Import rocket, mission, dragon and profile for the navlinks to function.
-import Rocket from "./components/rocket/rocket";
-import Dragon from "./components/dragon/dragon";
-import Mission from "./components/mission/mission";
-import Profile from "./components/myProfile/Profile";
-import Navbar from "./components/navbar/Navbar";
+import Rocket from './components/rocket/rocket';
+import Dragon from './components/dragon/dragon';
+import Mission from './components/mission/mission';
+import Profile from './components/myProfile/Profile';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,37 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
-// import logo from "../asset/_logo_.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <header className="panel-bg">
-      <div className="logo">
-        {/* <img src={logo}></img> */}
-        <span className="Logo-Text">Space Travelers Hub</span>
-      </div>
-      <ul>
-        <li>
-          <Link to="/rocket" className="Mission Text-Style-3">
-            Rocket
-          </Link>
-        </li>
-        <li>
-          <Link to="/mission" className="Mission Text-Style-3">
-            Missions
-          </Link>
-        </li>
-        <li>
-          <Link to="/dragon" className="Mission Text-Style-3">
-            Dragon
-          </Link>
-        </li>
-        <li>
-          <Link to="/profile" className="Myprofile Text-Style-3">
-            Myprofile
-          </Link>
-        </li>
-      </ul>
-    </header>
+    <>
+      <header className="panel-bg">
+        <div className="logoFrame">
+          <img className="logo" src="/planet.png" alt="space logo" />
+
+          <span className="Logo-Text">Space Travelers Hub</span>
+        </div>
+        <ul>
+          <li>
+            <Link to="/rocket" className="Mission Text-Style-3">
+              Rocket
+            </Link>
+          </li>
+          <li>
+            <Link to="/mission" className="Mission Text-Style-3">
+              Missions
+            </Link>
+          </li>
+          <li>
+            <Link to="/dragon" className="Mission Text-Style-3">
+              Dragon
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile" className="Myprofile Text-Style-3">
+              Myprofile
+            </Link>
+          </li>
+        </ul>
+      </header>
+      <hr className="horizon" />
+    </>
   );
-};
+}
 export default Navbar;
