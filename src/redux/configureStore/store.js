2b9fbef55b6d703ad76830f8756f2a9b610ab589
Petro Lesapiti <petrolesapiti@gmail.com>
2022-10-15 00:04:58 +0300

    Update the configure store.

--- src/redux/configureStore/store.js
+++ src/redux/configureStore/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
-import dragonReducer from '../dragon/dragon';
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import dragonReducer from "../dragon/dragon";
+import rocketSlice from "../rocket/rocketSlice";
 
 const store = configureStore({
   reducer: {
     dragon: dragonReducer,
+    rockets: rocketSlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
