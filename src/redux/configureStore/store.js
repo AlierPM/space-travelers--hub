eea8efcc15364f4a7c06317507118f5d8b3d7327
Alier Philip <alierditmaguet@gmail.com>
2022-10-15 16:08:13 +0200

    MissionReducer from the redux store

--- src/redux/configureStore/store.js
+++ src/redux/configureStore/store.js
@@ -1,12 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
-import logger from "redux-logger";
-import dragonReducer from "../dragon/dragon";
-import rocketSlice from "../rocket/rocketSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import dragonReducer from '../dragon/dragon';
+import rocketSlice from '../rocket/rocketSlice';
+import missionsReducer from '../mission/missionSlice';
 
 const store = configureStore({
   reducer: {
     dragon: dragonReducer,
     rockets: rocketSlice,
+    missions: missionsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
