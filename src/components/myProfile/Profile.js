8675781d05d93f666ec331e432493abcd6e920bd
Alier Philip <alierditmaguet@gmail.com>
2022-10-15 15:55:30 +0200

    Display missions on profile

--- src/components/myProfile/Profile.js
+++ src/components/myProfile/Profile.js
@@ -1,9 +1,13 @@
 /* eslint-disable */
-import { useSelector } from "react-redux";
-import "./profile.css";
+import { useSelector } from 'react-redux';
+import './profile.css';
 const Profile = () => {
   const rockets = useSelector((state) => state.rockets.rockets);
   const reservedRocket = rockets.filter((rocket) => rocket.reserved === true);
+  const missionList = useSelector((state) => state.missions);
+  const joinMission = missionList.filter(
+    (mission) => mission.reserved === true,
+  );
   return (
     <div className="profile">
       <table class="table">
@@ -13,10 +17,10 @@ const Profile = () => {
           </tr>
         </thead>
         <tbody>
-          {reservedRocket.map((rocket) => {
+          {joinMission.map((mission) => {
             return (
-              <td scope="col" id={rocket.id}>
-                {rocket.rocket_name}
+              <td scope="col" id={mission.id}>
+                {mission.mission_name}
               </td>
             );
           })}
